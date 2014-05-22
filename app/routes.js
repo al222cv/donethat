94ed4085765d8e03c4c27d02fa3c8a36e543d189
Andreas Larsson <larsson_andreas@live.se>
2014-05-22 23:42:29 +0200

    added appcache

--- app/routes.js
+++ app/routes.js
@@ -5,8 +5,14 @@ var ecstatic = require('ecstatic');
 
 module.exports = stack(
   route(/^\/$/, index),
+  route('/appcache', appcache),
   ecstatic({ root: __dirname + '/assets' })
 );
+
+function appcache(req, res){
+  res.setHeader('Content-Type', 'text/cache-manifest');
+  fs.createReadStream(__dirname + '/app.appcache').pipe(res);
+}
 
 function index(req, res){
   res.setHeader('Content-Type', 'text/html');
